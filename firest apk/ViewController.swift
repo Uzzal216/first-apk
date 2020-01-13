a293a2cb47d44eb326e2c170f8e4d30b44a93a2b
U.M. islam <u.m.hamidulislam@labs-mac.local>
2020-01-13 17:41:33 +0600

    session add

--- firest apk/ViewController.swift
+++ firest apk/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView1: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func callBtnPressed(_ sender: Any) {
+        
+        let session = URLSession.shared
+        let url = URL(string: "https://api.darksky.net/forecast/27b960d7886ff729d63ead9b912c75c8/37.8267,-122.4233")!
+        
+        let task = session.dataTask(with: url) { data, response, error in
+        
+        if let data = data {
+            print(data)
+            
+            self.textView1.text = String(data: data, encoding: .utf8)
+        }
+            
+            
+        }
+        task.resume()
+    }
+    
 }
 
